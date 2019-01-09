a2f3465a0a6330ac44ecb53f0a00cff5830bfdde
Jatzuk <jatzuk.22@gmail.com>
2019-01-09 16:12:53 +0300

    83

---
+++ src/main/kotlin/algorithms/easy/RemoveDuplicatesFromSortedList.kt
@@ -0,0 +1,24 @@
+package algorithms.easy
+
+/**
+ ** Created with passion and love
+ **    for project LeetCodePractice
+ **        by Jatzuk on 09.01.2019
+ **                                            *_____*
+ **                                           *_*****_*
+ **                                          *_(O)_(O)_*
+ **                                         **____V____**
+ **                                         **_________**
+ **                                         **_________**
+ **                                          *_________*
+ **                                           ***___***
+ */
+
+fun deleteDuplicates(head: ListNode?): ListNode? {
+    var current = head
+    while (current?.next != null) {
+        if (current.value == current.next?.value) current.next = current.next?.next
+        else current = current.next
+    }
+    return head
+}
