91c8c538d6003793dd2aeb9d77d7b9e733453916
jatzuk <jatzuk.22@gmail.com>
2019-01-12 16:33:57 +0300

    100

---
+++ src/test/kotlin/algorithms/easy/SameTree.kt
@@ -0,0 +1,44 @@
+package algorithms.easy
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ ** Created with passion and love
+ **    for project LeetCodePractice
+ **        by Jatzuk on 12.01.2019
+ **                                            *_____*
+ **                                           *_*****_*
+ **                                          *_(O)_(O)_*
+ **                                         **____V____**
+ **                                         **_________**
+ **                                         **_________**
+ **                                          *_________*
+ **                                           ***___***
+ */
+
+class SameTree {
+    val tree00 = TreeNode(1).apply {
+        left = TreeNode(2)
+        right = TreeNode(3)
+    }
+
+    val tree01 = TreeNode(1).apply {
+        left = TreeNode(2)
+        right = TreeNode(3)
+    }
+
+    val tree10 = TreeNode(1).apply {
+        left = TreeNode(2)
+    }
+
+    val tree11 = TreeNode(1).apply {
+        right = TreeNode(3)
+    }
+
+    @Test
+    fun isSameTree() {
+        assertEquals(true, isSameTree(tree00, tree01))
+        assertEquals(false, isSameTree(tree10, tree11))
+    }
+}
