3baf6c6deb9731a22cee5438ff37f28d781a976d
Jatzuk <jatzuk.22@gmail.com>
2019-02-02 11:49:21 +0300

    107

--- src/main/kotlin/algorithms/easy/BinaryTreeLevelOrderTraversalII.kt
+++ src/main/kotlin/algorithms/easy/BinaryTreeLevelOrderTraversalII.kt
@@ -2,7 +2,7 @@ package algorithms.easy
 
 import java.util.*
 
-/* 
+/*
  * Created with passion and love
  *    for project LeetCodePractice
  *        by Jatzuk on 01.02.2019
@@ -16,34 +16,32 @@ import java.util.*
  *                                           ***___***
  */
 
-fun levelOrderBottom(root: TreeNode?) = /*levelOrderDFS(root)*/ levelOrderBFS(root)
-
-private fun levelOrderDFS(root: TreeNode?): List<List<Int>> {
-    val stack = LinkedList<Pair<TreeNode?, Int>>()
-    val map = hashMapOf<Int, MutableList<Int>>()
-    if (root != null) stack.push(Pair(root, 0))
-
-    while (stack.isNotEmpty()) {
-        val elem = stack.pop()
-        val ptr = elem.first
-        if (ptr != null) {
-            val depth = elem.second
-            val row = map[depth] ?: arrayListOf()
-            row.add(ptr.value)
-            if (ptr.left != null) stack.push(Pair(ptr.left, depth + 1))
-            if (ptr.right != null) stack.push(Pair(ptr.right, depth + 1))
-            map[depth] = row
-        }
-    }
-
+fun levelOrderBottom(root: TreeNode?): List<List<Int>> {
     val list = arrayListOf<MutableList<Int>>()
-    for (i in map.size - 1 downTo 0) {
-        list.add(arrayListOf())
-        for (j in map[i]!!.size - 1 downTo 0) list[map.size - 1 - i].add(map[i]!![j])
-    }
+//    dfs(root, 1, list)
+    bfs(root, list)
     return list
 }
 
-private fun levelOrderBFS(root: TreeNode?): List<List<Int>> {
+private fun dfs(node: TreeNode?, depth: Int, list: MutableList<MutableList<Int>>) {
+    if (node == null) return
+    if (list.size < depth) list.add(0, arrayListOf())
+    list[list.size - depth].add(node.value)
+    dfs(node.left, depth + 1, list)
+    dfs(node.right, depth + 1, list)
+}
 
+private fun bfs(root: TreeNode?, list: MutableList<MutableList<Int>>) {
+    if (root == null) return
+    val queue: Queue<TreeNode> = LinkedList()
+    queue.add(root)
+    while (queue.isNotEmpty()) {
+        val row = arrayListOf<Int>()
+        for (i in 0 until queue.size) {
+            with(queue.peek().left) { if (this != null) queue.add(this) }
+            with(queue.peek().right) { if (this != null) queue.add(this) }
+            row.add(queue.poll().value)
+        }
+        list.add(0, row)
+    }
 }
