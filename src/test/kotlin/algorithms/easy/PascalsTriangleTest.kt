adffb0b5826b8cab919cc0bef4cd7c0c7b3bc161
Jatzuk <jatzuk.22@gmail.com>
2019-08-22 04:58:49 +0300

    118

---
+++ src/test/kotlin/algorithms/easy/PascalsTriangleTest.kt
@@ -0,0 +1,29 @@
+package algorithms.easy
+
+import org.junit.Test
+
+/*
+ * Created with passion and love 
+ *    for project LeetCodePractice
+ *        by Jatzuk on 22.08.2019
+ *                                            *_____*
+ *                                           *_*****_*
+ *                                          *_(O)_(O)_*
+ *                                         **____V____**
+ *                                         **_________**
+ *                                         **_________**
+ *                                          *_________*
+ *                                           ***___***
+ */
+
+class PascalsTriangleTest {
+    @Test
+    fun pascalsTriangle() {
+        pascalsTriangle(5).forEach(::println)
+    }
+
+    @Test
+    fun pascalsTriangleFunctional() {
+        pascalsTriangleFunctional(5).forEach(::println)
+    }
+}
