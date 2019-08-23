b3cd4c2be86a0534f6407b412304627c8191fe83
Jatzuk <jatzuk.22@gmail.com>
2019-08-23 21:49:20 +0300

    119

---
+++ src/main/kotlin/algorithms/easy/PascalsTriangleII.kt
@@ -0,0 +1,24 @@
+package algorithms.easy
+
+/*
+ * Created with passion and love 
+ *    for project LeetCodePractice
+ *        by Jatzuk on 22.08.2019
+ *                                            *_____*
+ *                                           *_*****_*
+ *                                          *_(O)_(O)_*
+ *                                         **____V____**
+ *                                         **_________**
+ *                                         **_________**
+ *                                          *_________*
+ *                                           ***___***
+ */
+
+fun getRow(rowIndex: Int): List<Int> {
+    val row = arrayListOf<Int>()
+    for (i in 0 until rowIndex + 1) {
+        row.add(1)
+        for (j in i - 1 downTo 1) row[j] = row[j] + row[j - 1]
+    }
+    return row
+}
