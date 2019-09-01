584a629d727bbb038a6200ab7a25eb967b792793
jatzuk <jatzuk.22@gmail.com>
2019-09-01 20:01:04 +0300

    122

--- src/test/kotlin/algorithms/easy/BestTimeToBuyAndSellStockIITest.kt
+++ src/test/kotlin/algorithms/easy/BestTimeToBuyAndSellStockIITest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 /* 
  * Created with passion and love
  *    for project LeetCodePractice
- *        by Jatzuk on 28.08.2019
+ *        by Jatzuk on 01.09.2019
  *                                            *_____*
  *                                           *_*****_*
  *                                          *_(O)_(O)_*
@@ -17,33 +17,28 @@ import org.junit.Test
  *                                           ***___***
  */
 
-class BestTimeToBuyAndSellStockTest {
+class BestTimeToBuyAndSellStockIITest {
     private val stocks1 = intArrayOf(7, 1, 5, 3, 6, 4)
     private val stocks2 = (7 downTo 0).toList().toIntArray()
     private val stocks3 = intArrayOf(2, 4, 1)
     private val stocks4 = intArrayOf(3, 2, 6, 5, 0, 3)
+    private val stocks5 = (1..5).toList().toIntArray()
 
     @Test
-    fun quadraticTimeTest() {
-        assertEquals(5, quadraticComplexity(stocks1))
-        assertEquals(0, quadraticComplexity(stocks2))
-        assertEquals(2, quadraticComplexity(stocks3))
-        assertEquals(4, quadraticComplexity(stocks4))
+    fun profitTest() {
+        assertEquals(7, maxProfitII(stocks1))
+        assertEquals(0, maxProfitII(stocks2))
+        assertEquals(2, maxProfitII(stocks3))
+        assertEquals(7, maxProfitII(stocks4))
+        assertEquals(4, maxProfitII(stocks5))
     }
 
     @Test
-    fun linearComplexity() {
-        assertEquals(5, maxProfit(stocks1))
-        assertEquals(0, maxProfit(stocks2))
-        assertEquals(2, maxProfit(stocks3))
-        assertEquals(4, maxProfit(stocks4))
-    }
-
-    @Test
-    fun functionalApproach() {
-        assertEquals(5, maxProfitFunctional(stocks1))
-        assertEquals(0, maxProfitFunctional(stocks2))
-        assertEquals(2, maxProfitFunctional(stocks3))
-        assertEquals(4, maxProfitFunctional(stocks4))
+    fun profitFunctionalTest() {
+        assertEquals(7, maxProfitIIFunctional(stocks1))
+        assertEquals(0, maxProfitIIFunctional(stocks2))
+        assertEquals(2, maxProfitIIFunctional(stocks3))
+        assertEquals(7, maxProfitIIFunctional(stocks4))
+        assertEquals(4, maxProfitIIFunctional(stocks5))
     }
 }
