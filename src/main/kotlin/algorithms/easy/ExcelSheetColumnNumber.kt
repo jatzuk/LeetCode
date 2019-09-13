172ed9b0fc03cde64417e3c1e3031f5d4f1596e1
jatzuk <jatzuk.22@gmail.com>
2019-09-13 20:36:42 +0300

    171

---
+++ src/main/kotlin/algorithms/easy/ExcelSheetColumnNumber.kt
@@ -0,0 +1,17 @@
+package algorithms.easy
+
+/* 
+ * Created with passion and love
+ *    for project LeetCodePractice
+ *        by Jatzuk on 13.09.2019
+ *                                            *_____*
+ *                                           *_*****_*
+ *                                          *_(O)_(O)_*
+ *                                         **____V____**
+ *                                         **_________**
+ *                                         **_________**
+ *                                          *_________*
+ *                                           ***___***
+ */
+
+fun titleToNumber(s: String) = s.fold(0) { acc, c -> acc * 26 + 1 + (c - 'A') }
