34ee2a3c1494498b9be12583d7e20fe58e836936
jatzuk <jatzuk.22@gmail.com>
2019-10-20 23:32:47 +0300

    219

---
+++ src/test/kotlin/algorithms/easy/ContainsDuplicateIIKtTest.kt
@@ -0,0 +1,34 @@
+package algorithms.easy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/*
+ * Created with passion and love
+ *    for project LeetCodePractice
+ *        by Jatzuk on 20/10/2019
+ *                                            *_____*
+ *                                           *_*****_*
+ *                                          *_(O)_(O)_*
+ *                                         **____V____**
+ *                                         **_________**
+ *                                         **_________**
+ *                                          *_________*
+ *                                           ***___***
+ */
+
+class ContainsDuplicateIIKtTest {
+    private val case1 = true to (intArrayOf(1, 2, 3, 1) to 3)
+    private val case2 = true to (intArrayOf(1, 0, 1, 1) to 1)
+    private val case3 = false to (intArrayOf(1, 2, 3, 1, 2, 3) to 2)
+    private val case4 = true to (intArrayOf(1, 1) to 2)
+
+    @Test
+    fun containsNearbyDuplicate() {
+        assertEquals(case1.first, containsNearbyDuplicate(case1.second.first, case1.second.second))
+        assertEquals(case2.first, containsNearbyDuplicate(case2.second.first, case2.second.second))
+        assertEquals(case3.first, containsNearbyDuplicate(case3.second.first, case3.second.second))
+        assertEquals(case4.first, containsNearbyDuplicate(case4.second.first, case4.second.second))
+    }
+}
