c27c78bd6c873d4e534ff5fbc796f4fe9e65a3df
jatzuk <jatzuk.22@gmail.com>
2019-10-24 22:58:04 +0300

    231

---
+++ src/test/kotlin/algorithms/easy/PowerOfTwoKtTest.kt
@@ -0,0 +1,68 @@
+package algorithms.easy
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+/*
+ * Created with passion and love
+ *    for project LeetCodePractice
+ *        by Jatzuk on 24/10/2019
+ *                                            *_____*
+ *                                           *_*****_*
+ *                                          *_(O)_(O)_*
+ *                                         **____V____**
+ *                                         **_________**
+ *                                         **_________**
+ *                                          *_________*
+ *                                           ***___***
+ */
+
+class PowerOfTwoKtTest {
+
+    @Test
+    fun isPowerOfTwoNaive() {
+        assertEquals(true, isPowerOfTwoNaive(4))
+        assertEquals(false, isPowerOfTwoNaive(5))
+        assertEquals(false, isPowerOfTwoNaive(218))
+        assertEquals(true, isPowerOfTwoNaive(1024))
+    }
+
+    @Test
+    fun isPowerOfTwoBitCount() {
+        assertEquals(true, isPowerOfTwoBitCount(4))
+        assertEquals(true, isPowerOfTwoBitCount(1024))
+        assertEquals(false, isPowerOfTwoBitCount(5))
+        assertEquals(false, isPowerOfTwoBitCount(218))
+        assertEquals(false, isPowerOfTwoBitCount(-256))
+    }
+
+    @Test
+    fun isPowerOfTwoIterative() {
+        assertEquals(true, isPowerOfTwoIterative(4))
+        assertEquals(true, isPowerOfTwoIterative(1024))
+        assertEquals(false, isPowerOfTwoIterative(5))
+        assertEquals(false, isPowerOfTwoIterative(218))
+        assertEquals(false, isPowerOfTwoIterative(-256))
+    }
+
+    @Test
+    fun isPowerOfTwoRecursive() {
+        assertEquals(true, isPowerOfTwoRecursive(4))
+        assertEquals(true, isPowerOfTwoRecursive(1024))
+        assertEquals(false, isPowerOfTwoRecursive(5))
+        assertEquals(false, isPowerOfTwoRecursive(218))
+        assertEquals(false, isPowerOfTwoRecursive(-256))
+    }
+
+    @Test
+    fun isPowerOfTwoBitwise() {
+        assertEquals(true, isPowerOfTwoBitwise(4))
+        assertEquals(true, isPowerOfTwoBitwise(1024))
+        assertEquals(false, isPowerOfTwoBitwise(5))
+        assertEquals(false, isPowerOfTwoBitwise(218))
+        assertEquals(false, isPowerOfTwoBitwise(-256))
+    }
+}
