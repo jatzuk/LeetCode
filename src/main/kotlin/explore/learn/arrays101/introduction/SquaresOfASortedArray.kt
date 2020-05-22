35531732d4c2d78c66167fda5c6ad0a63a73932e
jatzuk <jatzuk.22@gmail.com>
2020-05-22 13:40:23 +0300

    arrays 101 ex3

--- src/main/kotlin/explore/learn/arrays101/introduction/SquaresOfASortedArray.kt
+++ src/main/kotlin/explore/learn/arrays101/introduction/SquaresOfASortedArray.kt
@@ -25,24 +25,49 @@ fun sortedSquaresPointers(array: IntArray): IntArray {
     var negativeOffset = 0
     var sortedPointer = 0
 
-    while (array[negativeOffset + 1] < 0) negativeOffset++
-    var positiveOffset = negativeOffset + 1
+    while (negativeOffset < array.size && array[negativeOffset] < 0) negativeOffset++
+    var positiveOffset = --negativeOffset + 1
 
     while (negativeOffset >= 0 && positiveOffset < array.size) {
         val negativeCandidate = array[negativeOffset] * array[negativeOffset]
         val positiveCandidate = array[positiveOffset] * array[positiveOffset]
 
-        if (negativeCandidate < positiveCandidate) {
-            squares[sortedPointer++] = negativeCandidate
+        squares[sortedPointer++] = if (negativeCandidate < positiveCandidate) {
             negativeOffset--
+            negativeCandidate
         } else {
-            squares[sortedPointer++] = positiveCandidate
             positiveOffset++
+            positiveCandidate
         }
     }
 
     while (negativeOffset >= 0) squares[sortedPointer++] = array[negativeOffset] * array[negativeOffset--]
     while (positiveOffset < array.size) squares[sortedPointer++] = array[positiveOffset] * array[positiveOffset++]
+
+    return squares
+}
+
+// O(n)
+// O(n)
+fun sortedSquaresPointersBackTraverse(array: IntArray): IntArray {
+    val squares = IntArray(array.size)
+    var start = 0
+    var end = array.lastIndex
+
+    var backTraverseIndex = end
+    while (backTraverseIndex >= 0) {
+        val negativeCandidate = array[start] * array[start]
+        val positiveCandidate = array[end] * array[end]
+
+        squares[backTraverseIndex] = if (negativeCandidate > positiveCandidate) {
+            start++
+            negativeCandidate
+        } else {
+            end--
+            positiveCandidate
+        }
+        backTraverseIndex--
+    }
 
     return squares
 }
--- src/test/kotlin/explore/learn/arrays101/introduction/SquaresOfASortedArrayKtTest.kt
+++ src/test/kotlin/explore/learn/arrays101/introduction/SquaresOfASortedArrayKtTest.kt
@@ -21,16 +21,26 @@ class SquaresOfASortedArrayKtTest {
 
     private val case1 = intArrayOf(-4, -1, 0, 3, 10) to intArrayOf(0, 1, 9, 16, 100)
     private val case2 = intArrayOf(-7, -3, 2, 3, 11) to intArrayOf(4, 9, 9, 49, 121)
+    private val case3 = intArrayOf(-1) to intArrayOf(1)
 
     @Test
     fun sortedSquaresTest() {
         assertArrayEquals(case1.second, sortedSquares(case1.first))
         assertArrayEquals(case2.second, sortedSquares(case2.first))
+        assertArrayEquals(case3.second, sortedSquares(case3.first))
     }
 
     @Test
     fun sortedSquaresPointersTest() {
         assertArrayEquals(case1.second, sortedSquaresPointers(case1.first))
         assertArrayEquals(case2.second, sortedSquaresPointers(case2.first))
+        assertArrayEquals(case3.second, sortedSquaresPointers(case3.first))
+    }
+
+    @Test
+    fun sortedSquaresPointersBackTraverseTest() {
+        assertArrayEquals(case1.second, sortedSquaresPointersBackTraverse(case1.first))
+        assertArrayEquals(case2.second, sortedSquaresPointersBackTraverse(case2.first))
+        assertArrayEquals(case3.second, sortedSquaresPointersBackTraverse(case3.first))
     }
 }
